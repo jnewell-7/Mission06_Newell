b15ffe7b45ec708e8a3313bd2f73e3c16e82c4b6
jnewell-7 <jacksonnewell07@gmail.com>
2025-02-10 20:28:11 -0700

    tracked new files

---
+++ Mission06_Newell/Models/AddMovie.cs
@@ -0,0 +1,33 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Mission06_Newell.Models
+{
+    public class AddMovie
+    {
+        [Key] // Primary Key
+        public int MovieID { get; set; }
+
+        [Required(ErrorMessage = "Movie title is required")]
+        public string MovieTitle { get; set; }
+
+        [Required(ErrorMessage = "Category is required")]
+        public string Category { get; set; }
+
+        [Required(ErrorMessage = "Year is required")]
+        [Range(1888, 2100, ErrorMessage = "Enter a valid year between 1888 and 2100")]
+        public int Year { get; set; }
+
+        [Required(ErrorMessage = "Director is required")]
+        public string Director { get; set; }
+
+        [Required(ErrorMessage = "Rating is required")]
+        public string Rating { get; set; }
+
+        public bool? Edited { get; set; } // Nullable to allow deselection
+
+        public string LentTo { get; set; }
+
+        [StringLength(25, ErrorMessage = "Notes cannot exceed 25 characters")]
+        public string Notes { get; set; }
+    }
+}
